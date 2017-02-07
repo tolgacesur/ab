8942b6d27eceb36ff781b71b2858c40315d75382
tolgacesur <ttolgacesur@gmail.com>
2017-02-07 15:42:24 +0200

    düzenlendi

--- ESP8266_UserCongiguration.h
+++ ESP8266_UserCongiguration.h
@@ -1,15 +1,16 @@
 /*
+ * Header file for configuration
  * ESP8266_UserCongiguration.h
  *
  *  Created on: 05 Feb 2017
- *      Author: Orkan Murat CELIK
+ *      Author: Tolga CESUR
  */
 
 #ifndef APPLICATION_ESP8266_USERCONFIGUTATION_H_
 #define APPLICATION_ESP8266_USERCONFIGUTATION_H_
 
 
-#define wifi_ssid "Tolga"
-#define wifi_password "fsxz9097"
+#define wifi_ssid ""
+#define wifi_password "xxxx"
 
 #endif /* APPLICATION_ESP8266_USERCONFIGUTATION_H_ */
